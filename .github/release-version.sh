f5d219eb9aea737e3342e8b6fd4cf65a0c3b677c
Cody <luxueyu@nengli.chat>
2026-01-04 21:49:33 +0800

    更新 release-version.sh 脚本，修正 README.md 中 Maven 依赖版本更新的正则表达式

--- .github/release-version.sh
+++ .github/release-version.sh
@@ -34,7 +34,7 @@ mvn install
 echo "change version in README.md"
 cd "$CWD/.."
 # Update Maven dependency version
-sed -i '' -E '/<parent>/,/<\/parent>/ s/(<version>)[^<]+(<\/version>)/\1'"$NEW_VER"'\2/' README.md
+sed -i '' -E '/dino-sql-builder<\/artifactId>/{ n; s/(<version>)[^<]+(<\/version>)/\1'"$NEW_VER"'\2/; }' README.md
 # Update Gradle dependency version
 sed -i '' -E "s/(implementation 'cn\.dinodev:dino-sql-builder:)[0-9.]+'/\1$NEW_VER'/" README.md
 
